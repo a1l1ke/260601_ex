b2131d61a72e29b50782b47c42764ca01a455a09
a1l1ke <onetenthplease@gmail.com>
2026-06-01 14:44:47 +0900

    feat : 포켓몬 조회 함수 골격 추가

--- 02_script.js
+++ 02_script.js
@@ -7,13 +7,18 @@ const form = document.querySelector("#searchForm");
 form.addEventListener("submit", searchFormHandler);
 
 // Event Handler
-function searchFormHandler(event) {
+async function searchFormHandler(event) {
   event.preventDefault();
   console.log("searchFormHandler 확인");
+  const search = getFormData(event).get("search");
+  console.log("search", search);
+  const pokeData = await getPokeData(search);
+}
+
+function getFormData(event) {
   const formData = new FormData(event.target);
   console.log("formData", ...formData);
-  const search = formData.get("search");
-  console.log("search", search);
+  return formData;
 }
 
 // 데이터 조회
@@ -22,6 +27,10 @@ function searchFormHandler(event) {
 // https://pokeapi.co/docs/v2#pokemon
 // GET https://pokeapi.co/api/v2/pokemon/{id or name}/
 // form을 통해서 '번호'나 '영어이름' -> API로 호출
+
+async function getPokeData() {
+  console.log("getPokeData 확인");
+}
 
 // 데이터 변형
 
