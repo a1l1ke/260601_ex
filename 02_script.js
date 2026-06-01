a492e84d81a909a303ec87c0ffebbfb43f9cbefe
a1l1ke <onetenthplease@gmail.com>
2026-06-01 14:46:17 +0900

    feat : 포켓몬 조회 URL 생성 추가

--- 02_script.js
+++ 02_script.js
@@ -28,8 +28,10 @@ function getFormData(event) {
 // GET https://pokeapi.co/api/v2/pokemon/{id or name}/
 // form을 통해서 '번호'나 '영어이름' -> API로 호출
 
-async function getPokeData() {
+async function getPokeData(search) {
   console.log("getPokeData 확인");
+  const apiURL = `https://pokeapi.co/api/v2/pokemon/${search}`;
+  console.log("apiURL", apiURL);
 }
 
 // 데이터 변형
